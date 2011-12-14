109295ac359ca59fe05119df76d28a091519102d
Arjun Guha <arjun.guha@gmail.com>
2011-12-14 18:22:44 +0530

    expose opaque tree
    Inverts the parent relation to build a tree with edges from parent to
    children.

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,5 +1,5 @@
 module Tree (Tree, root, insert, member, lessThan, lessThanOrEq, chain,
-  descendingChain, depth, adjust, lookup, update, children) where
+  descendingChain, depth, adjust, lookup, update, children, expose) where
 
 import Prelude hiding (lookup)
 import Data.Map (Map)
@@ -7,23 +7,47 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Data.Map ((!))
-
+import qualified Data.Tree
 
 data Tree a b = 
-  Tree (Map a a)       -- ^parent relation
+  Tree a               -- ^root
+       (Map a a)       -- ^parent relation
        (Map a (Set a)) -- ^transitive closure of the parent relation
        (Map a (Set a)) -- ^child relationship
        (Map a b)       -- ^actual data
   deriving Show
 
 instance Ord a => Functor (Tree a) where
-  fmap f (Tree p tr child kvs) = Tree p tr child (fmap f kvs)
+  fmap f (Tree r p tr child kvs) = Tree r p tr child (fmap f kvs)
+
+-- |In the parent relation, the root does not appear as a key.
+-- In the child relation, the leaves do not appear as keys.
+invertMap :: (Ord a, Ord b)
+          => Map a b         -- ^parent relation
+          -> Map b [a]       -- ^child relation
+invertMap origMap = Map.foldrWithKey f Map.empty origMap
+  where f a b newMap = Map.insertWith (++) b [a] newMap
+
+toTree :: Ord a
+       => Map a [a]
+       -> Map a b
+       -> a
+       -> Data.Tree.Tree b
+toTree childMap kvs node = case Map.lookup node kvs of
+  Nothing  -> error "missing value"
+  Just val -> Data.Tree.Node val children
+    where children = case Map.lookup node childMap of
+            Nothing  -> []
+            Just lst -> map (toTree childMap kvs) lst
+  
+expose :: Ord a => Tree a b -> Data.Tree.Tree b
+expose tree@(Tree root parent _ _ kvs) = toTree (invertMap parent) kvs root
 
 root :: Ord a
      => a
      -> b
      -> Tree a b
-root k v = Tree Map.empty (Map.singleton k Set.empty) (Map.singleton k Set.empty)
+root k v = Tree k Map.empty (Map.singleton k Set.empty) (Map.singleton k Set.empty)
                 (Map.singleton k v)
 
 insert :: Ord a
@@ -32,9 +56,9 @@ insert :: Ord a
        -> a
        -> Tree a b
        -> Tree a b
-insert elt v pElt (Tree parent lt children kvs) =
+insert elt v pElt (Tree root parent lt children kvs) =
   case not (elt `Map.member` lt) && pElt `Map.member` lt of
-    True -> Tree parent' lt' children'' (Map.insert elt v kvs) where
+    True -> Tree root parent' lt' children'' (Map.insert elt v kvs) where
       parent' = Map.insert elt pElt parent
       lt' = Map.insert elt (Set.insert pElt (lt ! pElt)) lt
       children' = Map.insert pElt (Set.insert elt (children ! pElt)) children
@@ -46,7 +70,7 @@ lessThan :: Ord a
          -> a
          -> Tree a b
          -> Bool
-lessThan e p (Tree parent lt _ _) = 
+lessThan e p (Tree _ parent lt _ _) = 
   case (Map.member e lt) && (Map.member p lt) of
     True -> Set.member p (lt ! e)
     False -> error "element/parent not in lattice"
@@ -57,13 +81,13 @@ member :: Ord a
        => a
        -> Tree a b
        -> Bool
-member p (Tree parent lt _ _) = Map.member p lt
+member p (Tree _ parent lt _ _) = Map.member p lt
 
 chain :: Ord a
       => a
       -> Tree a b
       -> [(a, b)]
-chain from (Tree parent lt _ kvs) = f from where
+chain from (Tree _ parent lt _ kvs) = f from where
   f elt = case (Map.lookup elt parent, Map.lookup elt kvs) of
     (Nothing, Just v) -> [(elt, v)]
     (Just p, Just v) -> (elt, v) : (f p)
@@ -76,7 +100,7 @@ depth :: Ord a
       => a
       -> Tree a b
       -> Int
-depth elt (Tree _ lt _ _) =
+depth elt (Tree _ _ lt _ _) =
   case (Map.lookup elt lt) of
     Nothing -> error "element not in tree"
     Just x -> Set.size x
@@ -86,21 +110,21 @@ adjust :: Ord a
        -> a
        -> Tree a b
        -> Tree a b
-adjust f k (Tree m1 m2 m3 kvs) = Tree m1 m2 m3 (Map.adjust f k kvs)
+adjust f k (Tree m0 m1 m2 m3 kvs) = Tree m0 m1 m2 m3 (Map.adjust f k kvs)
 
 lookup :: Ord a => a -> Tree a b -> b
-lookup k (Tree _ _ _ kvs) = case Map.lookup k kvs of
+lookup k (Tree _ _ _ _ kvs) = case Map.lookup k kvs of
   Nothing -> error "fail"
   Just v -> v
 
 update :: Ord a => a -> b -> Tree a b -> Tree a b
-update k v (Tree m1 m2 m3 kvs) = Tree m1 m2 m3 (Map.insert k v kvs)
+update k v (Tree m0 m1 m2 m3 kvs) = Tree m0 m1 m2 m3 (Map.insert k v kvs)
 
 children :: Ord a
          => a
          -> Tree a b
          -> [(a, b)]
-children elt tr@(Tree parent _ chl kvs) = 
+children elt tr@(Tree _ parent _ chl kvs) = 
   case (Map.lookup elt chl) of
     Nothing -> []
     Just children -> map (\ x -> (x, lookup x tr)) (Set.toList children)
