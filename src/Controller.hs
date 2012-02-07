0911905d48a54726b248cef2470a85d7535647f0
Arjun Guha <arjun.guha@gmail.com>
2012-02-06 23:31:03 -0500

    controller learns routes for IP packets
    This is needed to Enqueue packets to port-associated queues

--- src/Controller.hs
+++ src/Controller.hs
@@ -7,11 +7,15 @@ import Data.Word
 import Control.Concurrent
 import qualified Data.ByteString as B
 import System.Environment
-import Nettle.OpenFlow.StrictPut -- as Put
+import Nettle.OpenFlow.StrictPut
 import FlowController (State)
 import Control.Concurrent.MVar
 import Base
 import Data.HashTable
+import Data.IORef
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.HList
 
 controllerMain :: MVar [CSMessage] -> Word16 -> IO ()
 controllerMain paneConfigState portNum = do
@@ -32,30 +36,70 @@ installActionsLoop ofpServer configMsgsVar = forever $ do
 
 handleSwitch :: SwitchHandle -> IO ()
 handleSwitch switch = do
-  let cfg = ExtQueueModify 1 [QueueConfig 6 [MinRateQueue (Enabled 500)]]
+  -- map of ports to destinations
+  learnedRoutes <- newIORef Map.empty
+  
+  let cfg = ExtQueueModify 1 [QueueConfig 3 [MinRateQueue (Enabled 700)],
+                              QueueConfig 6 [MinRateQueue (Enabled 500)]]
   sendToSwitch switch (10, cfg)
-  untilNothing (receiveFromSwitch switch) (messageHandler switch)
+  untilNothing (receiveFromSwitch switch) (messageHandler learnedRoutes switch)
   closeSwitchHandle switch
 
 
-messageHandler :: SwitchHandle -> (TransactionID, SCMessage) -> IO ()
-messageHandler switch (xid, scmsg) = do
-  putStrLn $ "Received: " ++ show scmsg
-  case scmsg of
-    PacketIn pkt        -> 
-      case enclosedFrame pkt of 
-        Left s -> putStrLn (s ++ ": " ++ show (B.unpack (packetData pkt)))
-        Right frame -> 
-          let flowEntry = AddFlow { match             = frameToExactMatch (receivedOnPort pkt) frame
-                                  , priority          = 32768
-                                  , actions           = [SendOutPort Flood]
-                                  , cookie            = 0
-                                  , idleTimeOut       = ExpireAfter 5
-                                  , hardTimeOut       = ExpireAfter 5
-                                  , notifyWhenRemoved = False
-                                  , applyToPacket     = bufferID pkt
-                                  , overlapAllowed    = True
-                                  } 
-          in sendToSwitch switch (xid, FlowMod flowEntry)
-    _                   -> return ()
+getPacketRoute pkt = case enclosedFrame pkt of
+  Right (HCons _ (HCons (IPInEthernet (HCons ipHdr _)) HNil)) -> 
+    Just (srcPort, srcIP, dstIP)
+      where srcPort = receivedOnPort pkt
+            srcIP = ipSrcAddress ipHdr
+            dstIP = ipDstAddress ipHdr
+  otherwise -> Nothing
+
+messageHandler :: IORef (Map IPAddress PortID)
+               -> SwitchHandle -> (TransactionID, SCMessage) -> IO ()
+messageHandler routesRef switch (xid, scmsg) = case scmsg of
+  PacketIn pkt -> case getPacketRoute pkt of
+    Nothing -> case enclosedFrame pkt of
+      Right frm -> do
+        let flowEntry = AddFlow { 
+                          match = frameToExactMatch (receivedOnPort pkt) frm,
+                          priority          = 32768,
+                          actions           = [SendOutPort Flood],
+                          cookie            = 0,
+                          idleTimeOut       = ExpireAfter 5,
+                          hardTimeOut       = ExpireAfter 5,
+                          notifyWhenRemoved = False,
+                          applyToPacket     = bufferID pkt,
+                          overlapAllowed    = True
+                        }
+        putStrLn "non-IP packet: flooding"
+        sendToSwitch switch (xid, FlowMod flowEntry)
+      Left _ -> putStrLn "PacketIn with an unrecognized frame."
+    Just (srcPort, srcIP, dstIP) -> do
+      -- learn
+      oldRoutes <- readIORef routesRef
+      let routes = Map.insert srcIP srcPort oldRoutes
+      writeIORef routesRef routes
+      -- lookup route
+      let action = case Map.lookup dstIP routes of
+                     Just dstPort -> [SendOutPort (PhysicalPort dstPort)]
+                     Nothing -> [SendOutPort Flood]
+      let flowEntry = AddFlow { 
+                        match = matchAny {
+                          inPort = Just srcPort, 
+                          srcIPAddress = (srcIP, maxPrefixLen),
+                          dstIPAddress = (dstIP, maxPrefixLen),
+                          ethFrameType = Just ethTypeIP
+                        },
+                        priority          = 32768,
+                        actions           = [SendOutPort Flood],
+                        cookie            = 0,
+                        idleTimeOut       = ExpireAfter 10,
+                        hardTimeOut       = ExpireAfter 60,
+                        notifyWhenRemoved = False,
+                        applyToPacket     = bufferID pkt,
+                        overlapAllowed    = True
+                      }
+      sendToSwitch switch (xid, FlowMod flowEntry)
+  otherwise -> do
+    putStrLn $ "unhandled packet " ++ show scmsg
 
