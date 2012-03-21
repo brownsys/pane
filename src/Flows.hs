f418eee02b9abb767e1cdafd993ba2235248e9bf
Arjun Guha <arjun.guha@gmail.com>
2012-03-21 10:20:00 -0400

    FlowGroup ADT wraps Nettle's Match
    Generalize when we figure out how to compile richer rules.

--- src/Flows.hs
+++ src/Flows.hs
@@ -5,7 +5,6 @@ module Flows
   , User
   , Host
   , Port
-  , union
   , intersection
   , null
   , all
@@ -20,7 +19,11 @@ import Data.Word (Word16)
 import Set (Set)
 import qualified Set as Set
 import Nettle.IPv4.IPAddress
+import qualified Nettle.IPv4.IPAddress as IPAddress
+import Nettle.OpenFlow.Match
+import qualified Nettle.OpenFlow.Match as Match
 
+-- import qualified FlowMatch as Z
 type User = String
 
 type Port = Word16
@@ -30,74 +33,84 @@ type Host = IPAddress
 data Flow = Flow (Maybe User) (Maybe User) 
                  (Maybe Port) (Maybe Port) 
                  (Maybe Host) (Maybe Host)
+  deriving (Eq, Ord, Show)
 
-isSubFlow :: FlowGroup -> FlowGroup -> Bool
-isSubFlow (FlowGroup fs1 fr1 fsp1 fdp1 fsh1 fdh1)
-          (FlowGroup fs2 fr2 fsp2 fdp2 fsh2 fdh2) =
-  Set.isSubsetOf fs1 fs2 &&
-  Set.isSubsetOf fr1 fr2 &&
-  Set.isSubsetOf fsp1 fsp2 &&
-  Set.isSubsetOf fdp1 fdp2 &&
-  Set.isSubsetOf fsh1 fsh2 &&
-  Set.isSubsetOf fdh1 fdh2
-
-isOverlapped :: FlowGroup -> FlowGroup -> Bool
-isOverlapped (FlowGroup fs1 fr1 fsp1 fdp1 fsh1 fdh1)
-             (FlowGroup fs2 fr2 fsp2 fdp2 fsh2 fdh2) =
-  not (Set.null (Set.intersection fs1 fs2)) &&
-  not (Set.null (Set.intersection fr1 fr2)) &&
-  not (Set.null (Set.intersection fsp1 fsp2)) &&
-  not (Set.null (Set.intersection fdp1 fdp2)) &&
-  not (Set.null (Set.intersection fsh1 fsh2)) &&
-  not (Set.null (Set.intersection fdh1 fdh2))
+data FlowGroup 
+  = FlowMatch Match
+  | Empty
+  deriving (Ord, Eq, Show)
 
 
-data FlowGroup = FlowGroup {
-  flowSend :: Set User,
-  flowRecv :: Set User,
-  flowSrcPort :: Set Port,
-  flowDstPort :: Set Port,
-  flowSrcHost :: Set Host,
-  flowDstHost :: Set Host 
-} deriving (Ord, Eq, Show)
+isSubFlow' :: Flow -> Flow -> Bool
+isSubFlow' (Flow su du sp dp sh dh) (Flow su' du' sp' dp' sh' dh') =
+  let (⊆) s1 s2 = case (s1, s2) of
+        (_, Nothing)      -> True
+        (Just x, Just y)  -> x == y
+        (Nothing, Just _) -> False
+    in su ⊆ su' && du ⊆ du' && sp ⊆ sp' && dp ⊆ dp' && sh ⊆ sh' && dh ⊆ dh'
 
-make = FlowGroup
+all = FlowMatch matchAny
 
-all = FlowGroup Set.all Set.all Set.all Set.all Set.all Set.all
+make :: Set User -> Set User -> Set Port -> Set Port -> Set Host -> Set Host
+     -> FlowGroup
+make su du sp dp sh dh = 
+  let port set = case Set.toList set of
+        Just [x]  -> Just (Just x)
+        Nothing   -> Just Nothing
+        otherwise -> Nothing
+      ip set = case Set.toList set of
+        Just [x]  -> Just (x, maxPrefixLen)
+        Nothing   -> Just (IPAddress 0,0)
+        otherwise -> Nothing
+      -- TODO(arjun): srcUser and dstUser are ignored
+      makeMatch _ _ sp dp sh dh = do
+       sp' <- port sp
+       dp' <- port dp
+       sh' <- ip dh
+       dh' <- ip dh
+       return $ matchAny { srcIPAddress = sh',  dstIPAddress = dh',
+                           srcTransportPort = sp', dstTransportPort = dp' }
+    in case makeMatch su du sp dp sh dh of
+         Just m -> FlowMatch m
+         Nothing -> error "flow group unsupported"
 
 flowInGroup :: Flow -> FlowGroup -> Bool
-flowInGroup (Flow srcUser dstUser srcPort dstPort srcHost dstHost)
-            (FlowGroup srcUserG dstUserG srcPortG dstPortG srcHostG dstHostG) =
-  srcUser `mem` srcUserG && dstUser `mem` dstUserG &&
-  srcPort `mem` srcPortG && dstPort `mem` dstPortG &&
-  srcHost `mem` srcHostG && dstHost `mem` dstHostG
-    where mem Nothing  set = set == Set.all
-          mem (Just e) set = e `Set.member` set
-
-union :: FlowGroup -> FlowGroup -> FlowGroup
-union (FlowGroup su1 du1 sp1 dp1 sh1 dh1) (FlowGroup su2 du2 sp2 dp2 sh2 dh2) =
-  FlowGroup (Set.union su1 su2) (Set.union du1 du2) 
-            (Set.union sp1 sp2) (Set.union dp1 dp2)
-             (Set.union sh1 sh2) (Set.union dh1 dh2)
+flowInGroup _                      Empty         = False
+flowInGroup (Flow _ _ sp dp sh dh) (FlowMatch m) =
+  sp `portIn` srcTransportPort m && dp `portIn` dstTransportPort m &&
+  sh `ipIn` srcIPAddress m && dh `ipIn` dstIPAddress m
+    where portIn _        Nothing   = True
+          portIn Nothing  (Just _)  = False
+          portIn (Just p) (Just p') = p == p'
+          ipIn _          (_, 0)   = True
+          ipIn Nothing    (_, len) = len == 0 
+          ipIn (Just ip)  pfx    = (ip, maxPrefixLen) `IPAddress.isSubset` pfx
 
 intersection :: FlowGroup -> FlowGroup -> FlowGroup
-intersection (FlowGroup su1 du1 sp1 dp1 sh1 dh1) 
-             (FlowGroup su2 du2 sp2 dp2 sh2 dh2) =
-  FlowGroup (Set.intersection su1 su2) (Set.intersection du1 du2)
-            (Set.intersection sp1 sp2) (Set.intersection dp1 dp2)
-             (Set.intersection sh1 sh2) (Set.intersection dh1 dh2)
+intersection _              Empty          = Empty
+intersection Empty          _              = Empty
+intersection (FlowMatch m1) (FlowMatch m2) = case Match.intersect m1 m2 of
+  Just m' -> FlowMatch m'
+  Nothing -> Empty
 
--- |'null fg' is true if 'fg' does not match any 'Flow'.
 null :: FlowGroup -> Bool
-null (FlowGroup su du sp dp sh dh) = 
-  Set.null su || Set.null du || Set.null sp || Set.null dp || 
-  Set.null sh || Set.null dh
+null Empty = True
+null _     = False
+
+isOverlapped :: FlowGroup -> FlowGroup -> Bool
+isOverlapped f1 f2 = not (null (intersection f1 f2))
+
+isSubFlow :: FlowGroup -> FlowGroup -> Bool
+isSubFlow Empty          _              = True
+isSubFlow _              Empty          = False
+isSubFlow (FlowMatch m1) (FlowMatch m2) = Match.subset m1 m2
 
 expand :: FlowGroup -> [Flow]
-expand (FlowGroup sendUser recvUser sendPort recvPort sendHost recvHost) = 
-  [ Flow su ru sp rp sh rh | su <- toList' sendUser, ru <- toList' recvUser,
-                             sp <- toList' sendPort, rp <- toList' recvPort,
-                             sh <- toList' sendHost, rh <- toList' recvHost ]
-    where toList' s = case Set.toList s of
-            Just lst -> map Just lst
-            Nothing -> [Nothing]
+expand Empty = []
+expand (FlowMatch (Match {srcIPAddress=sh,dstIPAddress=dh,
+                          srcTransportPort=sp,dstTransportPort=dp})) =
+  [Flow Nothing Nothing sp dp (ip sh) (ip dh)]
+    where ip (addr, 0) = Nothing
+          ip (addr, len)
+            | len == maxPrefixLen = Just addr
+            | otherwise           = error "prefixes unsupported"
