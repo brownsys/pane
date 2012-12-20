e6b20fcf2399b8e10dbdb00d226ff2a866674663
Andrew Ferguson <adf@cs.brown.edu>
2012-12-20 12:44:54 -0500

    add a simple perf monitoring service

--- src/Main.hs
+++ src/Main.hs
@@ -27,6 +27,7 @@ import Data.ConfigFile
 import Control.Monad.Error
 import Control.Exception
 import Management
+import qualified System.Remote.Monitoring as EKG
 
 data Argument
   = Test String
@@ -151,7 +152,7 @@ mainBody = do
   rawArgs <- getArgs
   let (args, options, errors) = getOpt RequireOrder argSpec rawArgs
   unless (null errors) $ do { mapM_ putStrLn errors; fail "bad args" }
-  action args
+  EKG.forkServer "localhost" 8000 >> action args
 
 shutdown = do
   return ()
